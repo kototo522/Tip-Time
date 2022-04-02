42d8111e5b7cf73d4640efe0b5de31f87488bf90
kototo522 <huyejiben@gmail.com>
2022-04-03 00:10:32 +0900

    add: コメントの追加

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -21,18 +21,23 @@ class MainActivity : AppCompatActivity() {
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
+
+        // 何も計算がされていない状態
         if (cost == null) {
             binding.tipResult.text = ""
             return
         }
 
+        // 選択に応じてチップのかけられる数を定義
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
 
+        // チップを計算
         var tip = tipPercentage * cost
+
         if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
